fb6accf78b86d8a3c07b1a7e429651493f8a0924
Md Rifat Bin Sumon <mdsumonpixel@gmail.com>
2026-03-17 23:49:15 +0600

    add JWT authentication with secure cookie and environment based config

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const port = process.env.PORT || 5000;
 const app = express();
 
 // MidleWare
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
+app.use(cookieParser());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5jgflna.mongodb.net/?appName=Cluster0`;
@@ -28,8 +36,48 @@ async function run() {
     const assignmentsCollection = database.collection("assignments");
     const submittedCollection = database.collection("submitted");
 
+    // jwt Authentication
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.JWT_SECRET, {
+        expiresIn: "7d",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+        })
+        .send({ success: true });
+    });
+
+    // Clear A token
+    app.post("/logout", (req, res) => {
+      res
+        .clearCookie("token", {
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+        })
+        .send({ success: true });
+    });
+
+    // MiddleWare Funtion
+    const verifyToken = (req, res, next) => {
+      const token = req.cookies?.token;
+      if (!token) {
+        return res.status(401).send({ message: "Unauthorized" });
+      }
+      jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "Unauthorized" });
+        }
+        req.user = decoded;
+        next();
+      });
+    };
+
     // Post Route For Assignments
-    app.post("/add-assignments", async (req, res) => {
+    app.post("/add-assignments", verifyToken, async (req, res) => {
       const assignments = req.body;
       const result = await assignmentsCollection.insertOne(assignments);
       res.send(result);
@@ -56,7 +104,7 @@ async function run() {
     });
 
     // Update a Specific  assignmnet
-    app.put("/assignments/:id", async (req, res) => {
+    app.put("/assignments/:id", verifyToken, async (req, res) => {
       const assignment = req.body;
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
@@ -74,7 +122,7 @@ async function run() {
     });
 
     // Delet a Specific assignment
-    app.delete("/assignments/:id", async (req, res) => {
+    app.delete("/assignments/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await assignmentsCollection.deleteOne(query);
@@ -84,7 +132,7 @@ async function run() {
     // All Submission Collection Starat
 
     // Post A submission collection
-    app.post("/submissions", async (req, res) => {
+    app.post("/submissions", verifyToken, async (req, res) => {
       const submission = req.body;
       const result = await submittedCollection.insertOne(submission);
       res.send(result);
@@ -98,32 +146,32 @@ async function run() {
     });
 
     // Get a specific submitted data
-    app.get('/submissions/:id', async(req, res) => {
+    app.get("/submissions/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await submittedCollection.findOne(query);
       res.send(result);
-    })
+    });
 
-  //  Update A specific submitted Data 
-  app.patch('/submissions/:id', async(req, res) => {
-    const id = req.params.id;
-     const { obtainedMarks, feedback, status } = req.body;
-    const filter = {_id: new ObjectId(id)};
-    const updatedDoc = {
-      $set: {obtainedMarks, feedback, status:"completed"}
-    }
-     const result = await submittedCollection.updateOne(filter, updatedDoc);
-  res.send(result);
-  })
+    //  Update A specific submitted Data
+    app.patch("/submissions/:id", verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const { obtainedMarks, feedback, status } = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: { obtainedMarks, feedback, status: "completed" },
+      };
+      const result = await submittedCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
 
-  // Get a MyAssigment Data
-  app.get('/my-assignments', async(req, res) => {
-    const email = req.query.email;
-    const query = {examinee_email: email};
-    const result = await submittedCollection.find(query).toArray();
-    res.send(result);
-  })
+    // Get a MyAssigment Data
+    app.get("/my-assignments", verifyToken, async (req, res) => {
+      const email = req.query.email;
+      const query = { examinee_email: email };
+      const result = await submittedCollection.find(query).toArray();
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
